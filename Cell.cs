3d1d58fbaa8463e60878cb532dd4f96dc6ffe1ed
ZagoskinVA <zagoskin.3@mail.ru>
2020-09-13 12:09:12 +0300

    test commit

---
+++ Cell.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Labirint
+{
+    class Cell
+    {
+        public int Step { get; set; } = 0;
+        public CellType CellType { get; set; } = CellType.Empty;
+        public int X { get; }
+        public int Y { get; }
+        public int CountOfBomb { get; }
+
+        public Cell(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public Cell(int x, int y, int countOfBomb)
+        {
+            X = x;
+            Y = y;
+            CountOfBomb = countOfBomb;
+        }
+        
+    }
+}
